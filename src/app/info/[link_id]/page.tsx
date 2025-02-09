bdc6ace892a033ef823dfee3584bc1450ad6ad01
Luiz Guilherme <xenubiagames@gmail.com>
2025-02-09 14:16:09 -0300

    delete: remove link id page info

--- src/app/info/[link_id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import ShortenedInfoServer from "@/components/shortened-info";
-
-interface Props {
-  params: { link_id: string };
-}
-
-export default function ShortenedInfoPage({ params }: Props) {
-  return <ShortenedInfoServer link_id={params.link_id} />;
-}
