17ad8d63b323379e032f6b45052d031263d58013
Luiz Guilherme <xenubiagames@gmail.com>
2025-02-10 09:28:51 -0300

    fix: simplifying code on db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,41 +1,19 @@
 import mongoose from "mongoose";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Por favor, defina a variável de ambiente MONGODB_URI no .env");
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Defina a variável de ambiente MONGODB_URI no .env");
 }
 
-const MONGODB_URI: string = process.env.MONGODB_URI;
+export async function connectToDatabase() {
+  if (mongoose.connection.readyState >= 1) {
+    return mongoose.connection;
+  }
 
-interface MongooseCache {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-}
-
-// Declaração global correta para evitar erro de propriedade inexistente
-declare global {
-  // biome-ignore lint/suspicious/noExplicitAny: Cache do Mongoose pode ser qualquer valor inicialmente
-  var mongooseCache: MongooseCache | undefined;
-}
-
-// Garante que `mongooseCache` exista globalmente
-globalThis.mongooseCache = globalThis.mongooseCache ?? { conn: null, promise: null };
-
-async function connectToDatabase() {
-  if (!globalThis.mongooseCache){
+  if (!MONGODB_URI){
     return;
   }
 
-  if (globalThis.mongooseCache.conn) {
-    return globalThis.mongooseCache.conn;
-  }
-
-  if (!globalThis.mongooseCache.promise) {
-    const opts = { bufferCommands: false };
-    globalThis.mongooseCache.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => mongoose);
-  }
-
-  globalThis.mongooseCache.conn = await globalThis.mongooseCache.promise;
-  return globalThis.mongooseCache.conn;
+  return mongoose.connect(MONGODB_URI);
 }
-
-export default connectToDatabase;
